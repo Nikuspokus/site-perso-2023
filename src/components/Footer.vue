8dc6d9a009372bb81c85766227a96cc1ad0997cc
nmarquillier <nmarquillier@norauto.com>
2022-10-02 17:10:16 +0200

    adding footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <footer class="text-center text-lg-start" style="background-color: #6e07f3">
+    <div class="container d-flex justify-content-center py-5">
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        style="background-color: #54456b; border-radius: 5rem"
+      >
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        style="background-color: #54456b; border-radius: 5rem"
+      >
+        <i class="fab fa-youtube"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        style="background-color: #54456b; border-radius: 5rem"
+      >
+        <i class="fab fa-instagram"></i>
+      </button>
+      <button
+        type="button"
+        class="btn btn-primary btn-lg btn-floating mx-2"
+        style="background-color: #54456b; border-radius: 5rem"
+      >
+        <i class="fab fa-twitter"></i>
+      </button>
+    </div>
+
+    <!-- Copyright -->
+    <!-- Copyright -->
+  </footer>
+  <div
+    class="text-center text-white p-3"
+    style="background-color: rgba(65, 29, 171, 0.9); width: 100%vh"
+  >
+    © 2020 Copyright:
+    <a class="text-white" href="/">nicolas marquillier</a>
+  </div>
+</template>
+
+<style></style>
